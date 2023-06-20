7c2d11285933ee467e42353f5c444fd615150170
GetTabled <stijnhertog@hotmail.nl>
2023-06-20 22:48:40 +0200

    Added Android Studio project: Empty Activity (Compose)

---
+++ app/src/main/java/com/table/tagalogquest/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.table.tagalogquest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.table.tagalogquest.ui.theme.TagalogQuestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TagalogQuestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("TagalogQuest")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TagalogQuestTheme {
+        Greeting("Android")
+    }
+}
