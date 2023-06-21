86a6b289cba05bc88f89be6d51662320399fcf64
GetTabled <stijnhertog@hotmail.nl>
2023-06-21 21:59:30 +0200

    Added Quest DAO

---
+++ app/src/main/java/com/table/tagalogquest/data/room/QuestDao.kt
@@ -0,0 +1,31 @@
+package com.table.tagalogquest.data.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+/*
+    Data Access Object for the Quest entity
+ */
+
+@Dao
+interface QuestDao {
+
+    // Create new Quest
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    suspend fun addQuest(quest: Quest)
+
+    // Get Quest by ID
+    @Query("SELECT * FROM quests WHERE id = :questId")
+    suspend fun getQuestById(questId: Long): Quest?
+
+    // Get all Quests
+    @Query("SELECT * FROM quests")
+    suspend fun getAllQuests(): List<Quest>
+
+    // Delete Quest
+    @Delete
+    suspend fun deleteQuest(quest: Quest)
+}
