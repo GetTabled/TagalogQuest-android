e9767832542422f1c5b826dc8363535ffba61664
GetTabled <stijnhertog@hotmail.nl>
2023-06-26 21:41:37 +0200

    Style current selected bottomNav entry

--- app/src/main/java/com/table/tagalogquest/ui/compose/BottomNavBarItem.kt
+++ app/src/main/java/com/table/tagalogquest/ui/compose/BottomNavBarItem.kt
@@ -1,10 +1,13 @@
 package com.table.tagalogquest.ui.compose
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,19 +21,46 @@ import com.table.tagalogquest.R
 @Composable
 fun BottomNavBarItem(isActive: Boolean, imageVector: Int, modifier: Modifier = Modifier) {
     Box(contentAlignment = Alignment.Center) {
-            Image(
-                imageVector = ImageVector.vectorResource(
-                    id = imageVector
-                ),
-                contentDescription = imageVector.toString()
-            )
+
             if (isActive) {
                 Box(
                     modifier = Modifier
-                        .background(Color(16, 79, 162, 100))
-                        .border(5.dp, Color(16, 79, 162, 255))
-                        .size(170.dp)
-                )
+                        .size(110.dp)
+                        .background(
+                            shape =  RoundedCornerShape(40.dp),
+                            color = Color(16, 79, 162, 100)
+                        )
+                        .border(
+                            shape = RoundedCornerShape(40.dp),
+                            border = BorderStroke(
+                                5.dp,
+                                Color(16, 79, 162, 255)
+                            )
+                        )
+                        .padding(25.dp)
+
+                ) {
+                    Image(
+                        imageVector = ImageVector.vectorResource(
+                            id = imageVector
+                        ),
+                        contentDescription = imageVector.toString()
+                    )
+                }
+            }
+            else {
+                Box(
+                    modifier = Modifier
+                        .size(110.dp)
+                        .padding(25.dp)
+                ) {
+                    Image(
+                        imageVector = ImageVector.vectorResource(
+                            id = imageVector
+                        ),
+                        contentDescription = imageVector.toString()
+                    )
+                }
             }
     }
 }
