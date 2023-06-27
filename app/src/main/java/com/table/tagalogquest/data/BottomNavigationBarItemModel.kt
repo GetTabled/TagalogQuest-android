7219ba48b81f7cf3d4cfc054f98dd1b9388ba67f
GetTabled <stijnhertog@hotmail.nl>
2023-06-27 22:04:04 +0200

    Added model for the BottomNavigationBar items

---
+++ app/src/main/java/com/table/tagalogquest/data/BottomNavigationBarItemModel.kt
@@ -0,0 +1,9 @@
+package com.table.tagalogquest.data
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationBarItemModel(
+    val name: String,
+    val route: String,
+    val icon: ImageVector
+)
