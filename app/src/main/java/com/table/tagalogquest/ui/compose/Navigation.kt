d17d64f6f963b35250d6f598bf333b37cb5ab7a8
GetTabled <stijnhertog@hotmail.nl>
2023-06-27 22:31:30 +0200

    Added BottomNavigationBar to the Navigation Composable

--- app/src/main/java/com/table/tagalogquest/ui/compose/Navigation.kt
+++ app/src/main/java/com/table/tagalogquest/ui/compose/Navigation.kt
@@ -1,9 +1,17 @@
 package com.table.tagalogquest.ui.compose
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.table.tagalogquest.R
+import com.table.tagalogquest.data.BottomNavigationBarItemModel
 import com.table.tagalogquest.data.NavigationRoutes
 
 /*
@@ -14,15 +22,37 @@ import com.table.tagalogquest.data.NavigationRoutes
 fun Navigation(
     navHostController: NavHostController
 ) {
-    NavHost(
-        navController = navHostController,
-        startDestination = NavigationRoutes.HOME
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        composable(NavigationRoutes.HOME) {
-            HomeScreen()
+        Box(modifier = Modifier.weight(1f)) {
+            NavHost(
+                navController = navHostController,
+                startDestination = NavigationRoutes.HOME
+            ) {
+                composable(NavigationRoutes.HOME) {
+                    HomeScreen()
+                }
+                composable(NavigationRoutes.SETTINGS) {
+                    SettingScreen()
+                }
+            }
         }
-        composable(NavigationRoutes.SETTINGS) {
-            SettingScreen()
-        }
+        BottomNavigationBar(
+            items = listOf(
+                BottomNavigationBarItemModel(
+                    name = "Home",
+                    route = NavigationRoutes.HOME,
+                    icon = ImageVector.vectorResource(id = R.drawable.ic_launcher_background)
+                ),
+                BottomNavigationBarItemModel(
+                    name = "Settings",
+                    route = NavigationRoutes.SETTINGS,
+                    icon = ImageVector.vectorResource(id = R.drawable.ic_launcher_background)
+                )
+            ),
+            navController = navHostController
+        )
     }
+
 }
