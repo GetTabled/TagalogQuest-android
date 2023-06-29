e9281e869065aad983e9131dc9739e298b7a2580
GetTabled <stijnhertog@hotmail.nl>
2023-06-29 19:09:07 +0200

    Added DataStore dependency

--- app/src/main/java/com/table/tagalogquest/MainActivity.kt
+++ app/src/main/java/com/table/tagalogquest/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.table.tagalogquest
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.compose.rememberNavController
 import com.table.tagalogquest.ui.compose.Navigation
 import com.table.tagalogquest.ui.theme.TagalogQuestTheme
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
